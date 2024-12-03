5e58ad6a2b59783daf3cc043095a5109676ab2fb
euler.amorim <euler.amorim@hotmail.com>
2024-12-02 21:44:25 -0300

    aula 14 - Subject e BehaviorSubject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObservablesComponent } from './components/observables/observables.component';
+import { SubjectComponent } from './components/subject/subject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ObservablesComponent
+    ObservablesComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/subject/subject.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-subject',
+  templateUrl: './subject.component.html',
+  styleUrl: './subject.component.scss',
+})
+export class SubjectComponent implements OnInit {
+  ngOnInit() {
+    this.initSubject();
+  }
+
+  initSubject() {
+    const subject = new Subject<number>();
+    const subject2 = new BehaviorSubject(0);
+
+    subject.subscribe({
+      next: (v) => console.log('subject: ', v),
+    });
+
+    subject.subscribe({
+      next: (v) => console.log('subject: ', v),
+    });
+
+    subject2.subscribe({
+      next: (v) => console.log('Behavior: ', v),
+    });
+
+    subject2.subscribe({
+      next: (v) => console.log('Behavior: ', v),
+    });
+
+    subject.next(10);
+    subject.next(12);
+
+    subject2.next(10);
+    subject2.next(12);
+  }
+}
