88d1be7dd3c4f20d95f27248257beda1b0c86b20
euler.amorim <euler.amorim@hotmail.com>
2024-12-10 17:47:19 -0300

    aula 17 - Of

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ObservablesComponent } from './components/observables/observables.component';
 import { SubjectComponent } from './components/subject/subject.component';
 import { FromComponent } from './components/creation-operators/from/from.component';
+import { OfComponent } from './components/creation-operators/of/of.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObservablesComponent,
     SubjectComponent,
-    FromComponent
+    FromComponent,
+    OfComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/creation-operators/of/of.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-of',
+  templateUrl: './of.component.html',
+  styleUrl: './of.component.scss',
+})
+export class OfComponent implements OnInit {
+  ngOnInit() {
+    this.operatorof();
+  }
+
+  operatorof() {
+    const arr = of([1, 2, 3, 4, 5]);
+    const string = of('Olá mundo!');
+    const multValue = of({ name: 'Danilo' }, true, function showName() {
+      return 'Danilo';
+    });
+
+    arr.subscribe((res) => console.log('Operator of: ', res));
+    string.subscribe((res) => console.log('Operator of: ', res));
+    multValue.subscribe((res) => console.log(res));
+  }
+}
